ba23254b319edbca3860e006398337f03d9469cc
nani-novu <naninovu@gmail.com>
2026-01-19 20:45:14 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('closeBtn').addEventListener('click', function () {
+    document.getElementById('popup').style.display = 'none';
+  });
+});
